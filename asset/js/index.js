15ea34d3ed7589abb743b37b26b22801eb320d32
adee012 <adedwiputra16@gmail.com>
2023-03-15 13:39:27 +0700

    [FIX] Perbaikan Kode

--- asset/js/index.js
+++ asset/js/index.js
@@ -2,8 +2,10 @@ const dasar = document.querySelector(".week1");
 const lanjutan = document.querySelector(".week2");
 const javaScript = document.querySelector(".week4");
 const hiden = document.querySelector(".hiden");
+const hiden2 = document.querySelector(".hiden2");
 const all = document.querySelector(".all-bab");
 const bab1 = document.querySelector(".bab1");
+const jScript = document.querySelector(".jScript");
 
 dasar.addEventListener("click", function () {
   all.style.display = "none";
@@ -14,3 +16,8 @@ lanjutan.addEventListener("click", function () {
   bab1.classList.add("hiden");
   all.style.display = "flex";
 });
+
+// javaScript.addEventListener("click", function () {
+//   all.style.display = "none";
+//   jScript.classList.remove("hiden2");
+// });
